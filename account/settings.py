6376c02dd628944ab65d631ebbbb0d8df4d457ce
Tomi Ogunsan <tomiafolayan@gmail.com>
2024-06-14 11:33:35 +0100

    removed commented lines

--- account/settings.py
+++ account/settings.py
@@ -83,8 +83,7 @@ DATABASES = {
 }
 
 AUTH_USER_MODEL = 'user.User'
-# Password validation
-# https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
